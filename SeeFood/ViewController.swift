e5ca16df5456102df308164ed06bcc67b65a5f43
Andrae J Henry <andrae.henry@icloud.com>
2020-05-21 09:20:34 -0400

    Update ViewController.swift

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
     }
     
