4369b7218359a517d24e5f6ff97f80508a2edb97
Andrae J Henry <andrae.henry@icloud.com>
2020-05-21 09:37:49 -0400

    Update Viewwcontroller. Classify Image from camera with Vission and CoreML

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
+        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
     
@@ -28,6 +28,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
        
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+          
+            guard  let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -35,6 +40,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
 
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Could not load CoreML Model")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard  let results =  request.results as? [VNClassificationObservation] else {
+                fatalError("Could not classify the image using Model")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+        try handler.perform([request])
+        } catch{
+            print(error)
+        }
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
